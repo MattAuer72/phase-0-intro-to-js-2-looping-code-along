4d9a2eefab021aeb71168aefe8fdf3d6acbddd19
Matthew Auer <matthewauer72@gmail.com>
2023-03-10 10:42:11 -0600

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const writeCards = (arr,event) => {
+  const messages = []
+  for(let i = 0; i < arr.length;i++) {
+    const greetingMessage = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+    messages.push(greetingMessage)
+  }
+return messages
+}
+
+console.log( writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown(){
+  let numCountDown=11;
+  while(numCountDown--){
+    console.log(numCountDown)
+  }
+}
